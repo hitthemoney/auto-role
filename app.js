71faa9d194fa3b180e7fe2ca614ad5475a03f5dd
hitthemoney <asher@grusd.com>
2020-06-21 09:32:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,16 +28,17 @@ bot.on("message", message => {
         let msg = message.content;
         try {
             let level = msg.split(" level ")[1].split("!")[0],
-                user = message.mentions.members.first();
+                user = message.mentions.members.first(),
+                role = "";
             console.log([level, user])
             if (level >= 30) {
-                let role = message.guild.roles.cache.find(role => role.name === 'Ultimate Member');
+                role = message.guild.roles.cache.find(role => role.name === 'Ultimate Member');
                 message.author.roles.add(role);
             } else if (level >= 5) {
-                let role = message.guild.roles.cache.find(role => role.name === 'Cool Member');
+                role = message.guild.roles.cache.find(role => role.name === 'Cool Member');
                 message.author.roles.add(role);
             } else if (level >= 15) {
-                let role = message.guild.roles.cache.find(role => role.name === 'Active Member');
+                role = message.guild.roles.cache.find(role => role.name === 'Active Member');
                 message.author.roles.add(role);
             }
         } catch (err) {
